a4d2f0abc95945981bd74852686179841a101af5
Andrew Meier <booyankees13@gmail.com>
2016-02-12 09:21:58 -0500

    first commit

---
+++ Dartmouth Coach/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Dartmouth Coach
+//
+//  Created by Andrew Meier on 2/12/16.
+//  Copyright © 2016 CS89. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
