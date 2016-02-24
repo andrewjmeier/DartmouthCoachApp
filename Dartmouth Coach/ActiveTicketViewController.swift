4eb3d1d2ca1c4c9b1edca543d74eb8f0f3b4bfc7
Andrew Meier <booyankees13@gmail.com>
2016-02-24 11:59:35 -0500

    add cool patterns and stuff

--- Dartmouth Coach/ActiveTicketViewController.swift
+++ Dartmouth Coach/ActiveTicketViewController.swift
@@ -11,23 +11,62 @@ import UIKit
 class ActiveTicketViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var patternView: UIImageView!
+    
+    var velX:CGFloat = 1
+    var velY:CGFloat = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        let date = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM/dd/YY hh:mm"
+        dateLabel.text = dateFormatter.stringFromDate(date)
+        
+        let random = arc4random_uniform(3)
+        var fileName = ""
+        switch (random) {
+            case 0:
+                fileName = "pattern"
+                break
+            case 1:
+                fileName = "pattern2"
+                break
+            case 2:
+                fileName = "patter3"
+                break
+            default:
+                fileName = "pattern"
+                break
+        }
+        let img = UIImage(named: fileName)
+        patternView.image = img
+        
+                // Do any additional setup after loading the view.
     }
 
     override func viewDidAppear(animated: Bool) {
         dateLabel.frame.origin = CGPoint(x: 100, y: 100)
-        NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("timerFired"), userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(0.05, target:self, selector: Selector("timerFired"), userInfo: nil, repeats: true)
     }
     
     func timerFired() {
 //        dateLabel.frame.origin = CGPoint(x: dateLabel.frame.origin.x + 1, y: dateLabel.frame.origin.y + 1)
-        dateLabel.frame.origin.x += 1
-        dateLabel.frame.origin.y += 1
-        print("fired!")
+        let pos = dateLabel.frame
+        if (pos.origin.x + pos.size.width > patternView.frame.origin.x + patternView.frame.size.width) {
+            velX = velX * -1
+        } else if (pos.origin.x < patternView.frame.origin.x) {
+            velX = velX * -1
+        }
+        
+        if (pos.origin.y + pos.size.height > patternView.frame.origin.y + patternView.frame.size.height) {
+            velY = velY * -1
+        } else if (pos.origin.y < patternView.frame.origin.y) {
+            velY = velY * -1
+        }
+        dateLabel.frame.origin.x += velX
+        dateLabel.frame.origin.y += velY
     }
     
     override func didReceiveMemoryWarning() {
